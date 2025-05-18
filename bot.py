91c1f1b64fa2564a2bc6022bb1d2fc44f3b17bad
DarkDeath2796 <gelepukinandrew@gmail.com>
2025-05-18 12:24:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 from sys import argv
+from .main import LaylaAI
+
+
+
+layla: LaylaAI = LaylaAI()
 
 
 TOKEN: str = argv[1]
@@ -10,6 +15,13 @@ BOT = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready() -> None:
     print(f"Logged in as {BOT.user.name}")
 
+async def on_message(message: discord.Message) -> None:
+    if message.author == BOT.user:
+        return
 
+    if message.content.channel.id == argv[2]:
+        prompt = message.content
+        response = layla.process_message(prompt, message.author.name)
+        await message.channel.send(response)
 
 BOT.run(TOKEN)
